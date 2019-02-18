b8de66ab0dbe07a58414d5b402f8f0878a02b4f3
Jennifer Cheng <ashowga@gmail.com>
2019-02-18 18:12:18 +0800

    Add delete eagle confirmation

--- resources/js/components/Eagles/DeleteEagle.vue
+++ resources/js/components/Eagles/DeleteEagle.vue
@@ -1,6 +1,39 @@
 <template>
     <div :id="`DeleteEagle-${eagle.id}`">
-        <button v-on:click="deleteEagle(eagle.id)" class="btn btn-danger">Delete</button>
+
+        <!-- Button to Open the Modal -->
+        <button v-on:click="deleteEagleConfirmation(eagle.id)" type="button" class="btn btn-danger" data-toggle="modal" :data-target="`#DeleteEagleModal-${eagle.id}`">
+            Delete
+        </button>
+
+        <!-- The Modal -->
+        <div class="modal fade" :id="`DeleteEagleModal-${eagle.id}`">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                
+                    <!-- Modal Header -->
+                    <div class="modal-header">
+                    <h4 class="modal-title">Delete Confirmation</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    
+                    <!-- Modal body -->
+                    <div class="modal-body">
+                    <!-- Photo by Andreas Barth from Pexels -->
+                    <img style="width: 100%;" :src="'/img/upset_eagle.jpg'">
+                    Are you sure you want to delete {{ eagle.name }}?
+                    </div>
+                    
+                    <!-- Modal footer -->
+                    <div class="modal-footer">
+                        <button v-on:click="deleteEagle(eagle.id)" class="btn btn-danger" data-dismiss="modal">Killing it. Goodbye ;(</button>
+                        <button type="button" class="btn btn-primary" data-dismiss="modal">It's Okay, maybe next time.</button>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -21,6 +54,9 @@ export default {
         }
     },
     methods: {
+        deleteEagleConfirmation(id){
+
+        },
         deleteEagle(id){
             this.$store.dispatch('deleteEagle', {
                 id
