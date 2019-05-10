8a9f4dc6317e5176e72d0107f1b109f131132e8d
Jennifer Cheng <ashowga@gmail.com>
2019-05-10 11:58:16 +0800

    - Move navbar inside a container

--- resources/js/components/MainApp.vue
+++ resources/js/components/MainApp.vue
@@ -3,36 +3,38 @@
         <!-- Wrapper container for footer position set up -->
         <div class="wrapper">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="/">Braviary</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul v-if="userNotLoggedIn" class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/sign-up">Sign Up</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/log-in">Log In</router-link>
-                        </li>
-                    </ul>
-                    <ul v-else class="navbar-nav ml-auto">
-                        <li v-if="isAdmin" class="nav-item">
-                            <router-link to="/zookeeper/eagles">All Eagles</router-link>
-                        </li>
-                        <li v-if="isAdmin" class="nav-item">
-                            <router-link v-if="isAdmin" to="/zookeeper/users">All Users</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link to="/eagles">Eagles</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link to="/me">Profile</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link v-on:click.native="userLogOut" to="/">Log Out</router-link>
-                        </li>
-                    </ul>
+                <div class="container">
+                    <a class="navbar-brand" href="/">Braviary</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul v-if="userNotLoggedIn" class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <router-link class="nav-link" to="/sign-up">Sign Up</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link class="nav-link" to="/log-in">Log In</router-link>
+                            </li>
+                        </ul>
+                        <ul v-else class="navbar-nav ml-auto">
+                            <li v-if="isAdmin" class="nav-item">
+                                <router-link to="/zookeeper/eagles">All Eagles</router-link>
+                            </li>
+                            <li v-if="isAdmin" class="nav-item">
+                                <router-link v-if="isAdmin" to="/zookeeper/users">All Users</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link to="/eagles">Eagles</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link to="/me">Profile</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link v-on:click.native="userLogOut" to="/">Log Out</router-link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
             <!-- Page content -->
